84bf92dcd9ff672586f5d79912df3884e2a8d029
host1l3 <timvitkulevskij@gmail.com>
2025-05-30 00:19:32 +0700

    Create TotalDisciplines.cpp

---
+++ Initiation/TotalDisciplines.cpp
@@ -0,0 +1,42 @@
+#include "TotalDisciplines.h"
+
+std::ostream& operator<<(std::ostream& out, const TotalDiscip& d) {
+	out << "Current Disceplines: ";
+	for (Discipline data : d.getDisciplines()) {
+		out << "\t" << data;
+	}
+	return out;
+}
+
+std::vector <Discipline> TotalDiscip::getDisciplines() const {
+	return disciplines;
+}
+
+void TotalDiscip::addDisc(Discipline d) {
+	disciplines.push_back(d);
+	uniqueDisc();
+}
+void TotalDiscip::addDisc(short TCU, std::string name) {
+	disciplines.push_back(Discipline{ TCU, name });
+	uniqueDisc();
+}
+
+bool TotalDiscip::uniqueDisc() {
+	bool duplicatesFound = false;
+
+	for (auto it = disciplines.begin(); it != disciplines.end(); ++it) {
+		for (auto jt = it + 1; jt != disciplines.end(); ) {
+			if (*it == *jt) {
+				jt = disciplines.erase(jt);
+				duplicatesFound = true;
+			}
+			else ++jt;
+		}
+	}
+
+	return !duplicatesFound;
+}
+
+void TotalDiscip::deleteDisc(Discipline d) {
+	disciplines.erase(std::remove(disciplines.begin(), disciplines.end(), d), disciplines.end());
+}
