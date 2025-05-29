31cf9565ade79e0f3d526684f2f74b2c107644b1
host1l3 <timvitkulevskij@gmail.com>
2025-05-30 00:12:33 +0700

    Create Discipline.h

---
+++ Headers/Discipline.h
@@ -0,0 +1,30 @@
+#ifndef DISCIPLINE
+#define DISCIPLINE
+
+#include "Needed_libs.h"
+
+class Discipline {
+private:
+	short CCU;
+	std::string discName;
+public:
+	// Конструкторы
+	Discipline();
+	Discipline(short TCU, std::string discName);
+
+	// Оператор присваивания и сравнения
+	Discipline& operator=(const Discipline& d);
+	bool operator==(const Discipline& d) const;
+	friend std::ostream& operator<<(std::ostream& out, const Discipline& d);
+
+	// Геттеры
+	short getCurrentCU() const;
+	std::string getDiscName() const;
+
+	// Сеттеры значений
+	void setCurrentCU(short ccu);
+	void setDName(std::string name);
+};
+
+
+#endif
