c8afa57fd66026a2641b5049e62bf3942d63edde
host1l3 <timvitkulevskij@gmail.com>
2025-05-30 00:14:16 +0700

    Create TotalDisciplines.h

---
+++ Headers/TotalDisciplines.h
@@ -0,0 +1,28 @@
+#ifndef TOTAL_DISCIPLINES
+#define TOTAL_DISCIPLINES
+
+#include "Discipline.h"
+#include "Needed_libs.h"
+
+class TotalDiscip {
+private:
+	std::vector <Discipline> disciplines;
+public:
+	// Оператор вывода в поток
+	friend std::ostream& operator<<(std::ostream& out, const TotalDiscip& d);
+
+	// Обращение к вектору значений
+	std::vector <Discipline> getDisciplines() const;
+
+	// Методы добавления элементов в контейнер
+	void addDisc(Discipline d);
+	void addDisc(short TCU, std::string name);
+
+	// Метод проверки элементов контейнера на уникальность
+	bool uniqueDisc();
+
+	// Удаление по дисциплины значению
+	void deleteDisc(Discipline d);
+};
+
+#endif
