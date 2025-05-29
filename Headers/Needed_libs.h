2bd33fc37a1446277273a863267bd2c43059faa2
host1l3 <timvitkulevskij@gmail.com>
2025-05-30 00:11:53 +0700

    Create Needed_libs.h

---
+++ Headers/Needed_libs.h
@@ -0,0 +1,14 @@
+#ifndef NEEDED_LIBS
+#define NEEDED_LIBS
+
+#include "MAPS.h"
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <sstream>   
+#include <stdexcept>
+#include <functional>
+
+#endif 
