543ecea976fdcd426cf1a6a6527dd300c7c27055
host1l3 <timvitkulevskij@gmail.com>
2025-05-31 20:14:31 +0700

    Update Cipher.h

--- Headers/Cipher.h
+++ Headers/Cipher.h
@@ -7,12 +7,14 @@ class Cipher {
 private:
 	std::string pt1, pt2, pt3;
 	bool isNum(std::string s);
+	void splitByDots(std::vector<std::string>& vec, const std::string& input);
 public:
 	// Полный конструктор
 	Cipher(std::string p1 = "00", std::string p2 = "00", std::string p3 = "00");
 
 	// Установка полного шифра
 	void setParts(std::string p1, std::string p2, std::string p3);
+	void setFullCipher(std::string cipher);
 
 	// Оператор сравнения и присвоения
 	Cipher& operator=(const Cipher& other);
